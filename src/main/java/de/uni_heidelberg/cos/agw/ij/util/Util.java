7b193aecbcc175e65a28be3f23b26445615d8aee
bhoeckendorf <b.hoeckendorf@web.de>
2012-08-28 14:48:18 +0200

    Added utility methods to copy LUT and calibration, round and calibrate grid and subgrid positions.

--- src/main/java/de/uni_heidelberg/cos/agw/ij/util/Util.java
+++ src/main/java/de/uni_heidelberg/cos/agw/ij/util/Util.java
@@ -19,10 +19,15 @@
 package de.uni_heidelberg.cos.agw.ij.util;
 
 import ij.ImagePlus;
+import ij.measure.Calibration;
 import ij.process.ByteProcessor;
 import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import ij.process.ShortProcessor;
+import javax.vecmath.Point2d;
+import javax.vecmath.Point2i;
+import javax.vecmath.Point3d;
+import javax.vecmath.Point3i;
 
 public class Util {
 
@@ -51,5 +56,85 @@ public class Util {
             default:
                 return new ByteProcessor(width, height);
         }
+    }
+
+    public static void copyLutAndCalibration(final ImagePlus from, final ImagePlus to) {
+        to.getProcessor().setLut(from.getProcessor().getLut());
+        to.copyScale(from);
+    }
+
+    public static Point2i getGridPosition(final Point2d point) {
+        Point2i pixel = new Point2i();
+        pixel.x = (int) Math.round(point.x);
+        pixel.y = (int) Math.round(point.y);
+        return pixel;
+    }
+
+    public static Point2d calibratePoint(final Point2i point, final Calibration calibration) {
+        Point2d calibratedPoint = new Point2d();
+        calibratedPoint.x = point.x * calibration.pixelWidth;
+        calibratedPoint.y = point.y * calibration.pixelHeight;
+        return calibratedPoint;
+    }
+
+    public static Point2d calibratePoint(final Point2d point, final Calibration calibration) {
+        Point2d calibratedPoint = new Point2d();
+        calibratedPoint.x = point.x * calibration.pixelWidth;
+        calibratedPoint.y = point.y * calibration.pixelHeight;
+        return calibratedPoint;
+    }
+
+    public static Point2d getSubGridPosition(final Point2d point, final Calibration calibration) {
+        Point2d pixel = new Point2d();
+        pixel.x = point.x / calibration.pixelWidth;
+        pixel.y = point.y / calibration.pixelHeight;
+        return pixel;
+    }
+
+    public static Point2i getGridPosition(final Point2d point, final Calibration calibration) {
+        Point2i pixel = new Point2i();
+        pixel.x = (int) Math.round(point.x / calibration.pixelWidth);
+        pixel.y = (int) Math.round(point.y / calibration.pixelHeight);
+        return pixel;
+    }
+
+    public static Point3i getGridPosition(final Point3d point) {
+        Point3i pixel = new Point3i();
+        pixel.x = (int) Math.round(point.x);
+        pixel.y = (int) Math.round(point.y);
+        pixel.z = (int) Math.round(point.z);
+        return pixel;
+    }
+
+    public static Point3d calibratePoint(final Point3i point, final Calibration calibration) {
+        Point3d calibratedPoint = new Point3d();
+        calibratedPoint.x = point.x * calibration.pixelWidth;
+        calibratedPoint.y = point.y * calibration.pixelHeight;
+        calibratedPoint.z = (point.z - 1) * calibration.pixelDepth;
+        return calibratedPoint;
+    }
+
+    public static Point3d calibratePoint(final Point3d point, final Calibration calibration) {
+        Point3d calibratedPoint = new Point3d();
+        calibratedPoint.x = point.x * calibration.pixelWidth;
+        calibratedPoint.y = point.y * calibration.pixelHeight;
+        calibratedPoint.z = (point.z - 1) * calibration.pixelDepth;
+        return calibratedPoint;
+    }
+
+    public static Point3d getSubGridPosition(final Point3d point, final Calibration calibration) {
+        Point3d pixel = new Point3d();
+        pixel.x = point.x / calibration.pixelWidth;
+        pixel.y = point.y / calibration.pixelHeight;
+        pixel.z = point.z / calibration.pixelDepth + 1;
+        return pixel;
+    }
+
+    public static Point3i getGridPosition(final Point3d point, final Calibration calibration) {
+        Point3i pixel = new Point3i();
+        pixel.x = (int) Math.round(point.x / calibration.pixelWidth);
+        pixel.y = (int) Math.round(point.y / calibration.pixelHeight);
+        pixel.z = (int) Math.round(point.z / calibration.pixelDepth) + 1;
+        return pixel;
     }
 }
