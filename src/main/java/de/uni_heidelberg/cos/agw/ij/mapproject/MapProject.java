c2050ae13f2e898ec64cb1ff1110fcfa98f957e5
bhoeckendorf <b.hoeckendorf@web.de>
2013-01-17 00:37:35 +0100

    MapProject works with specified radius range and uses Util.IntensityProjector.

--- src/main/java/de/uni_heidelberg/cos/agw/ij/mapproject/MapProject.java
+++ src/main/java/de/uni_heidelberg/cos/agw/ij/mapproject/MapProject.java
@@ -1,13 +1,13 @@
 /*
  * This file is part of the COS AGW ImageJ plugin bundle.
  * https://github.com/bhoeckendorf/cos-agw_ij
- * 
+ *
  * Copyright 2012, 2013  B. Hoeckendorf <b.hoeckendorf at web dot de>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -16,9 +16,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package de.uni_heidelberg.cos.agw.ij.mapproject;
 
+import de.uni_heidelberg.cos.agw.ij.util.IntensityProjector;
+import de.uni_heidelberg.cos.agw.ij.util.Util;
 import fiji.util.gui.GenericDialogPlus;
 import ij.IJ;
 import ij.ImagePlus;
@@ -27,86 +28,103 @@ import ij.process.ByteProcessor;
 import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import ij.process.ShortProcessor;
-
 import javax.vecmath.Point3i;
 
-import de.uni_heidelberg.cos.agw.ij.util.Util;
-
-
 public class MapProject implements PlugInFilter {
-	
-	private ImagePlus inputImp;
-	private Sphere sphere;
 
-	
-	@Override
-	public int setup(String args, ImagePlus imp) {
-		inputImp = imp;
-		return STACK_REQUIRED + DOES_8G + DOES_16 + DOES_32;
-	}
+    private ImagePlus inputImp;
+    private Sphere sphere;
+    private IntensityProjector intensityProjector;
 
+    @Override
+    public int setup(String args, ImagePlus imp) {
+        inputImp = imp;
+        return STACK_REQUIRED + DOES_8G + DOES_16;
+    }
 
-	@Override
-	public void run(ImageProcessor inputIp) {
-		GenericDialogPlus dialog = new GenericDialogPlus("World Map Project");
-		dialog.addNumericField("Center_x", 480, 0, 4, "voxel");
-		dialog.addNumericField("Center_y", 430, 0, 4, "voxel");
-		dialog.addNumericField("Center_z", 380, 0, 4, "voxel");
-		dialog.addNumericField("Radius", 330, 0, 4, "voxel");
-		dialog.addNumericField("Pole_offset", 0, 1, 4, "degrees");
-		dialog.addNumericField("Zero_meridian_offset", 0, 1, 4, "degrees");
-		dialog.addNumericField("Scaling", 1, 1, 4, "x");
-		dialog.showDialog();
-		if(dialog.wasCanceled())
-			return;
+    @Override
+    public void run(ImageProcessor inputIp) {
+        GenericDialogPlus dialog = new GenericDialogPlus("Map Project");
+        dialog.addNumericField("Center_x", 480, 0, 4, "voxel");
+        dialog.addNumericField("Center_y", 430, 0, 4, "voxel");
+        dialog.addNumericField("Center_z", 380, 0, 4, "voxel");
+        dialog.addNumericField("Radius_inner", 330, 0, 4, "voxels");
+        dialog.addNumericField("Radius_outer", 330, 0, 4, "voxels");
+        dialog.addNumericField("Plane_position", 0.75, 1, 4, "0-1");
+        dialog.addNumericField("Pole_offset", 0, 1, 4, "degrees");
+        dialog.addNumericField("Zero_meridian_offset", 0, 1, 4, "degrees");
+        dialog.addNumericField("Scaling", 1, 1, 4, "x");
+        dialog.showDialog();
+        if (dialog.wasCanceled()) {
+            return;
+        }
 
-		int centerX = (int)Math.round(dialog.getNextNumber());
-		int centerY = (int)Math.round(dialog.getNextNumber());
-		int centerZ = (int)Math.round(dialog.getNextNumber());
-		int radius = (int)Math.round(dialog.getNextNumber());
-		double poleOffset = dialog.getNextNumber();
-		double zeroMeridianOffset = dialog.getNextNumber();
-		double scaling = dialog.getNextNumber();
-		
-		Sphere.setCenter(new Point3i(centerX, centerY, centerZ));
-		Sphere.setPoleOffset(poleOffset);
-		Sphere.setZeroMeridianOffset(zeroMeridianOffset);
-		Sphere.setIntensityProjector(new IntensityProjector(inputImp.getImageStack()));
+        int centerX = (int) Math.round(dialog.getNextNumber());
+        int centerY = (int) Math.round(dialog.getNextNumber());
+        int centerZ = (int) Math.round(dialog.getNextNumber());
+        int radiusInner = (int) Math.round(dialog.getNextNumber());
+        int radiusOuter = (int) Math.round(dialog.getNextNumber());
+        double planePosition = dialog.getNextNumber();
+        double poleOffset = dialog.getNextNumber();
+        double zeroMeridianOffset = dialog.getNextNumber();
+        double scaling = dialog.getNextNumber();
 
-		IJ.showStatus("World Map Project ...");
-		sphere = new Sphere(radius);
-		ImageProcessor outputIp = projectPlateCaree(scaling);
-		String filenameParams = String.format("-map-cx%d-cy%d-cz%d-r%d-po%.2f-zo%.2f-s%.2f", centerX, centerY, centerZ, radius, poleOffset, zeroMeridianOffset, scaling);
-		ImagePlus outputImp = new ImagePlus(Util.addToFilename(inputImp.getTitle(), filenameParams), outputIp);
-		outputImp.show();
-	}
-	
-	
-	private ImageProcessor projectPlateCaree(double scaling) {
-		int outputSizeX = (int)Math.round(sphere.getVoxelCountAtEquator() * scaling);
-		int outputSizeY = (int)Math.round(outputSizeX / 2);
-		ImageProcessor outputIp = getNewOutputIp(outputSizeX, outputSizeY);
-		for (int x = 0; x < outputSizeX; x++) {
-			for (int y = 0; y < outputSizeY; y++) {
-				double theta = ( 2 * Math.PI / outputSizeX ) * x;
-				double phi = ( Math.PI / outputSizeY ) * y;
-				double value = sphere.getValueOfPhiTheta(phi, theta);
-				outputIp.putPixelValue(x, y, value);
-			}
-			IJ.showProgress(x + 1, outputSizeX);
-		}
-		return outputIp;
-	}
-	
-	
-	private ImageProcessor getNewOutputIp(int inputSizeX, int inputSizeY) {
-		final int bits = inputImp.getBitDepth();
-		if (bits == 16)
-			return new ShortProcessor(inputSizeX, inputSizeY);
-		else if (bits == 32)
-			return new FloatProcessor(inputSizeX, inputSizeY);
-		else
-			return new ByteProcessor(inputSizeX, inputSizeY);
-	}
+        final int planeRadius = (int) Math.round(radiusInner + planePosition * (radiusOuter - radiusInner));
+        sphere = new Sphere(new Point3i(centerX, centerY, centerZ), planeRadius);
+        sphere.setPoleOffset(poleOffset);
+        sphere.setZeroMeridianOffset(zeroMeridianOffset);
+        intensityProjector = new IntensityProjector(inputImp);
 
+        IJ.showStatus("Map Project ...");
+        ImageProcessor outputIp = projectPlateCaree(scaling, radiusInner, radiusOuter);
+        String filenameParams = String.format("-map-cx%d-cy%d-cz%d-r%d-po%.2f-zo%.2f-s%.2f", centerX, centerY, centerZ, planeRadius, poleOffset, zeroMeridianOffset, scaling);
+        ImagePlus outputImp = new ImagePlus(Util.addToFilename(inputImp.getTitle(), filenameParams), outputIp);
+        outputImp.show();
+    }
+
+//    private ImageProcessor projectPlateCaree(double scaling) {
+//        int outputSizeX = (int) Math.round(sphere.getVoxelCountAtEquator() * scaling);
+//        int outputSizeY = (int) Math.round(outputSizeX / 2);
+//        ImageProcessor outputIp = getNewOutputIp(outputSizeX, outputSizeY);
+//        for (int x = 0; x < outputSizeX; x++) {
+//            for (int y = 0; y < outputSizeY; y++) {
+//                double theta = (2 * Math.PI / outputSizeX) * x;
+//                double phi = (Math.PI / outputSizeY) * y;
+//                double value = sphere.getValueOfPhiTheta(phi, theta);
+//                outputIp.putPixelValue(x, y, value);
+//            }
+//            IJ.showProgress(x + 1, outputSizeX);
+//        }
+//        return outputIp;
+//    }
+    
+    private ImageProcessor projectPlateCaree(double scaling, double innerRadius, double outerRadius) {
+        int outputSizeX = (int) Math.round(sphere.getVoxelCountAtEquator() * scaling);
+        int outputSizeY = (int) Math.round(outputSizeX / 2);
+        ImageProcessor outputIp = getNewOutputIp(outputSizeX, outputSizeY);
+        double[] radii = {innerRadius, outerRadius};
+        for (int x = 0; x < outputSizeX; x++) {
+            for (int y = 0; y < outputSizeY; y++) {
+                double theta = (2 * Math.PI / outputSizeX) * x;
+                double phi = (Math.PI / outputSizeY) * y;
+                Point3i[] points = sphere.sphericalToCartesianGrid(phi, theta, radii);
+                intensityProjector.set(points[0], points[1]);
+                int value = intensityProjector.getMaximum();
+                outputIp.putPixelValue(x, y, value);
+            }
+            IJ.showProgress(x + 1, outputSizeX);
+        }
+        return outputIp;
+    }
+
+    private ImageProcessor getNewOutputIp(int inputSizeX, int inputSizeY) {
+        final int bits = inputImp.getBitDepth();
+        if (bits == 16) {
+            return new ShortProcessor(inputSizeX, inputSizeY);
+        } else if (bits == 32) {
+            return new FloatProcessor(inputSizeX, inputSizeY);
+        } else {
+            return new ByteProcessor(inputSizeX, inputSizeY);
+        }
+    }
 }
--- src/main/java/de/uni_heidelberg/cos/agw/ij/mapproject/Sphere.java
+++ src/main/java/de/uni_heidelberg/cos/agw/ij/mapproject/Sphere.java
@@ -1,13 +1,13 @@
 /*
  * This file is part of the COS AGW ImageJ plugin bundle.
  * https://github.com/bhoeckendorf/cos-agw_ij
- * 
+ *
  * Copyright 2012, 2013  B. Hoeckendorf <b.hoeckendorf at web dot de>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -16,147 +16,116 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package de.uni_heidelberg.cos.agw.ij.mapproject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-
 import javax.media.j3d.Transform3D;
 import javax.vecmath.Matrix3d;
 import javax.vecmath.Point3d;
 import javax.vecmath.Point3i;
 import javax.vecmath.Vector3d;
 
-
 public class Sphere {
-	
-	private static final Point3i center = new Point3i(0, 0, 0);
-	private static final Vector3d vector = new Vector3d(center.x, center.y, center.z);
-	private int radius = 0;
-	private static IntensityProjector projector;
-	private Map<Point3i, Double> valuesCache = new HashMap<Point3i, Double>();
-	private static double
-		poleOffset = 0,
-		zeroMeridianOffset = 0;
-	private static final Matrix3d poleOffsetMatrix = new Matrix3d();
-	private static Transform3D transform;
 
-	
-	public Sphere(int radius) {
-		setRadius(radius);
-		poleOffsetMatrix.rotY(poleOffset);
-	}
-	
-		
-	public static void setCenter(Point3i point) {
-		center.x = point.x - 1;
-		center.y = point.y - 1;
-		center.z = point.y;
-		vector.x = center.x;
-		vector.y = center.y;
-		vector.z = center.z;
-		transform = new Transform3D(poleOffsetMatrix, vector, 1d);
-	}
+    private final Point3i center = new Point3i(0, 0, 0);
+    private final Vector3d vector = new Vector3d(center.x, center.y, center.z);
+    private final int radius;
+    private double poleOffset = 0,
+            zeroMeridianOffset = 0;
+    private final Matrix3d poleOffsetMatrix = new Matrix3d();
+    private Transform3D transform;
 
-	
-	public static Point3i getCenter() {
-		return new Point3i(center.x + 1, center.y + 1, center.z);
-	}
-	
-	
-	public void setRadius(int radius) {
-		this.radius = radius;
-	}
-	
-	
-	public int getRadius() {
-		return radius;
-	}
-	
-	
-	public static void setIntensityProjector(IntensityProjector projector) {
-		Sphere.projector = projector; 
-	}
-	
+    public Sphere(Point3i center, final int radius) {
+        center.x = center.x - 1;
+        center.y = center.y - 1;
+        center.z = center.z;
+        vector.x = center.x;
+        vector.y = center.y;
+        vector.z = center.z;
+        this.radius = radius;
+        transform = new Transform3D(poleOffsetMatrix, vector, 1d);
+        poleOffsetMatrix.rotY(poleOffset);
+    }
 
-	public static void setPoleOffset(double degrees) {
-		poleOffset = Math.toRadians(degrees);
-		poleOffsetMatrix.rotY(poleOffset);
-		transform = new Transform3D(poleOffsetMatrix, vector, 1d);
-	}
-	
-	
-	public static double getPoleOffset() {
-		return Math.toDegrees(poleOffset);
-	}
+    public void setPoleOffset(double degrees) {
+        poleOffset = Math.toRadians(degrees);
+        poleOffsetMatrix.rotY(poleOffset);
+        transform = new Transform3D(poleOffsetMatrix, vector, 1d);
+    }
 
-	
-	public static void setZeroMeridianOffset(double degrees) {
-		zeroMeridianOffset = Math.toRadians(degrees);
-	}
-	
-	
-	public static double getZeroMeridianOffset() {
-		return Math.toDegrees(zeroMeridianOffset);
-	}
-	
-	
-	public int getVoxelCountAtEquator() {
-		return voxelsOfPhi(Math.PI / 2, 0.00001).size();
-	}
-	
-	
-	private List<Point3i> voxelsOfPhi(double phi, double stepSizeInRadians) {
-		Set<Point3i> set = new HashSet<Point3i>();
-		List<Point3i> list = new ArrayList<Point3i>();
-		for (double theta = 0; theta < 2*Math.PI; theta += stepSizeInRadians) {
-			Point3i cartesian = sphericalToCartesianGrid(phi, theta);
-			if (set.add(cartesian))
-				list.add(cartesian);
-		}
-		return list;
-	}
-	
-	
-	private List<Point3i> voxelsOfTheta(double theta, double stepSizeInRadians) {
-		Set<Point3i> set = new HashSet<Point3i>();
-		List<Point3i> list = new ArrayList<Point3i>();
-		for (double phi = 0; phi < 2*Math.PI; phi += stepSizeInRadians) {
-			Point3i cartesian = sphericalToCartesianGrid(phi, theta);
-			if (set.add(cartesian))
-				list.add(cartesian);
-		}
-		return list;
-	}
+    public double getPoleOffset() {
+        return Math.toDegrees(poleOffset);
+    }
 
-	
-	public double getValueOfPhiTheta(double phi, double theta) {
-		Point3i point = sphericalToCartesianGrid(phi, theta);
-		Double value = valuesCache.get(point);
-		if (value == null) {
-			value = projector.maxAlongRay(center, point);
-			valuesCache.put(point, value);
-		}
-		return value;
-	}
+    public void setZeroMeridianOffset(double degrees) {
+        zeroMeridianOffset = Math.toRadians(degrees);
+    }
 
+    public double getZeroMeridianOffset() {
+        return Math.toDegrees(zeroMeridianOffset);
+    }
 
-	public Point3i sphericalToCartesianGrid(double phi, double theta) {
-		theta += zeroMeridianOffset;
-		final double radiusSinPhi = radius * Math.sin(phi);
-		double x = radiusSinPhi * Math.cos(theta);
-		double y = radiusSinPhi * Math.sin(theta);
-		double z = radius * Math.cos(phi);
+    public int getVoxelCountAtEquator() {
+        return voxelsOfPhi(Math.PI / 2, 0.00001).size();
+    }
 
-		Point3d point = new Point3d(x, y, z);
-		transform.transform(point);
-		
-		return new Point3i((int)Math.round(point.x), (int)Math.round(point.y), (int)Math.round(point.z));
-	}
+    private List<Point3i> voxelsOfPhi(double phi, double stepSizeInRadians) {
+        Set<Point3i> set = new HashSet<Point3i>();
+        List<Point3i> list = new ArrayList<Point3i>();
+        for (double theta = 0; theta < 2 * Math.PI; theta += stepSizeInRadians) {
+            Point3i cartesian = sphericalToCartesianGrid(phi, theta);
+            if (set.add(cartesian)) {
+                list.add(cartesian);
+            }
+        }
+        return list;
+    }
 
+    private List<Point3i> voxelsOfTheta(double theta, double stepSizeInRadians) {
+        Set<Point3i> set = new HashSet<Point3i>();
+        List<Point3i> list = new ArrayList<Point3i>();
+        for (double phi = 0; phi < 2 * Math.PI; phi += stepSizeInRadians) {
+            Point3i cartesian = sphericalToCartesianGrid(phi, theta);
+            if (set.add(cartesian)) {
+                list.add(cartesian);
+            }
+        }
+        return list;
+    }
+
+    public Point3i sphericalToCartesianGrid(final double phi, double theta) {
+        theta += zeroMeridianOffset;
+        final double radiusSinPhi = radius * Math.sin(phi);
+        double x = radiusSinPhi * Math.cos(theta);
+        double y = radiusSinPhi * Math.sin(theta);
+        double z = radius * Math.cos(phi);
+
+        Point3d point = new Point3d(x, y, z);
+        transform.transform(point);
+
+        return new Point3i((int) Math.round(point.x), (int) Math.round(point.y), (int) Math.round(point.z));
+    }
+
+    public Point3i[] sphericalToCartesianGrid(final double phi, double theta, final double[] radii) {
+        theta += zeroMeridianOffset;
+        final double sinPhi = Math.sin(phi);
+        final double cosPhi = Math.cos(phi);
+        final double sinTheta = Math.sin(theta);
+        final double cosTheta = Math.cos(theta);
+        Point3i[] points = new Point3i[radii.length];
+        for (int i = 0; i < radii.length; ++i) {
+            final double radius = radii[i];
+            final double radiusSinPhi = radius * sinPhi;
+            double x = radiusSinPhi * cosTheta;
+            double y = radiusSinPhi * sinTheta;
+            double z = radius * cosPhi;
+            Point3d point = new Point3d(x, y, z);
+            transform.transform(point);
+            points[i] = new Point3i((int) Math.round(point.x), (int) Math.round(point.y), (int) Math.round(point.z));
+        }
+        return points;
+    }
 }
